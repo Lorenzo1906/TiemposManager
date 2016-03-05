88813d11e9d41f82c21460a2e0ce78b950e21672
Lorenzo1906 <Lorenzo1906@gmail.com>
2016-03-04 20:38:28 -0600

    Dependency update and elimination of AppLoader

--- src/main/java/net/lorenzo/tiemposmanager/MainApp.java
+++ src/main/java/net/lorenzo/tiemposmanager/MainApp.java
@@ -15,19 +15,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class MainApp extends Application {
 
     public static void main(String[] args) {
-        LauncherImpl.launchApplication(MainApp.class, AppPreloader.class, args);
+        LauncherImpl.launchApplication(MainApp.class, args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        notifyPreloader(new ProgressNotification(10d));
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TiemposManagerAppFactory.class);
-        notifyPreloader(new ProgressNotification(50d));
         MainPresenter mainPresenter = context.getBean(MainPresenter.class);
         mainPresenter.setTabs();
         ReportePresenter reportePresenter = context.getBean(ReportePresenter.class);
         reportePresenter.inicializeComboBox();
-        notifyPreloader(new ProgressNotification(90d));
 
         Scene scene = new Scene(mainPresenter.getView(), 590, 419);
         scene.getStylesheets().add("/styles/fxmlapp.css");
@@ -36,7 +33,6 @@ public class MainApp extends Application {
         stage.setResizable(false);
         stage.initStyle(StageStyle.UTILITY);
 
-        notifyPreloader(new StateChangeNotification(StateChangeNotification.Type.BEFORE_START));
         Platform.runLater(stage::show);
 
         stage.show();
